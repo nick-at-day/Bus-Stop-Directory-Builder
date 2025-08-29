4276d40c63a9b744a2977b15785793207268a6e1
Nick Seitz <nickseitz@gmail.com>
2025-08-28 21:10:50 -0400

    little by little- building up classes and build logic

--- File Operations.py
+++ File Operations.py
@@ -16,6 +16,15 @@ class Photo_Entity:
 
     def getKeywordAtIndex(self, index):
         return self.__keywords[index]
+    
+    def getAllKeywords(self):
+        return self.__keywords
+    
+    def getNumberOfKeys(self):
+        return len(self.__keywords)
+    
+    def returnStopNumberKeywordPair(self):
+        return [[self.__stopNumber, kw] for kw in self.__keywords]
 
 
 class Keyword_Entity:
@@ -23,9 +32,44 @@ class Keyword_Entity:
         self.__label = label
         self.__stopsReferencing = stopsReferencing if stopsReferencing is not None else []
 
+    def getStopsFromKeyword(self):
+        return self.__stopsReferencing
+
+photos = []
+
 with ExifToolHelper() as et:
     for files in os.listdir("fileInputs/"):
         filePath = os.path.join("fileInputs/" + files)
-        for d in et.get_tags([filePath], ["Keywords"]):
-            for k, v in d.items():
-                print(f"Dict: {k} = {v}")
+        for d in et.get_tags([filePath], ["IPTC:Keywords"]):
+            keywords = d.get("IPTC:Keywords", [])
+            if isinstance(keywords, str):
+                keywords = [keywords]
+            fileName = d["SourceFile"]
+            kw_clean = []
+            for kw in keywords:
+                if kw=="onPage":
+                    break
+                if kw == "edited":
+                    break
+                kw_clean.append(kw.replace("_",""))
+            photos.append(Photo_Entity(fileName,kw_clean))
+
+
+# for localKeys in photos:
+#     print(localKeys.getAllKeywords())
+
+all_keys = set()
+
+for k in photos:
+    for keyword in k.getAllKeywords():
+        all_keys.add(keyword)
+
+keyDict = []
+
+for keys in all_keys:
+    keyDict.append(Keyword_Entity(keys, ""))
+
+print(keyDict[1])
+
+# for k in photos:
+#     print(k.getAllKeywords())
