4a95f83addb8ecdfacab2c953e23e58e18ba82b3
Nick Seitz <nickseitz@gmail.com>
2025-08-29 21:59:03 -0400

    it's working i am exultant : o

--- File Operations.py
+++ File Operations.py
@@ -4,6 +4,11 @@ import json
 from pathlib import Path
 from exiftool import ExifToolHelper
 from pprint import pprint
+from itertools import zip_longest
+import lorem
+import random
+
+sourceFilepath = "/Users/nicholasseitz/Pictures/Update Directory/"
 
 class Photo_Entity:
     def __init__(self, stopNumber=None, stopKeywords=None, index=None):
@@ -35,13 +40,12 @@ class Photo_Entity:
             "Stop Number": self.__stopNumber,
             "keywords": self.__keywords             
         }
+    
     @classmethod
-    def dumpToJson(cls, photos, filename="keyList.json"):
-        allData = [photo.toDict() for photo in photos]
+    def dumpToJson(cls, photos, filename="stopList.json"):
+        allData = [photo.toDict() for photo in photos.values()]
         with open(filename, "w") as o:
             json.dump(allData, o, indent=4)
-
-
 
 class Keyword_Entity:
     def __init__(self, label=None, stopsReferencing=None):
@@ -56,6 +60,9 @@ class Keyword_Entity:
     def returnStops(self):
         return self.__stopsReferencing
 
+    def getLabel(self):
+        return self.__label
+
     def getStopsFromKeyword(self):
         if isinstance(self.__stopsReferencing, str):
             self.__stopsReferencing = [self.__stopsReferencing]
@@ -68,50 +75,81 @@ class Keyword_Entity:
         if stop not in self.__stopsReferencing:
             self.__stopsReferencing.append(stop)
 
-photos = []
+    def toDict(self):
+        return {
+            "Keyword": self.__label,
+            "Stops": self.__stopsReferencing
+        }
+
+    @classmethod
+    def dumpKeysToJson(cls, keys, filename="keyList.json"):
+        allData = [photo.toDict() for photo in keys.values()]
+        with open(filename, "w") as o:
+            json.dump(allData, o, indent=4)
+
+photos = {}
+all_keys = set()
+keyDict = {} 
 
 with ExifToolHelper() as et:
-    for files in os.listdir("fileInputs/"):
-        filePath = os.path.join("fileInputs/" + files)
+    for files in os.listdir(sourceFilepath):
+        filePath = os.path.join(sourceFilepath + files)
         fileName = os.path.splitext(files)
-        for d in et.get_tags([filePath], ["IPTC:Keywords"]):
-            keywords = d.get("IPTC:Keywords", [])
-            if isinstance(keywords, str):
-                keywords = [keywords]
-            fileName = fileName[0]
-            kw_clean = []
-            for kw in keywords:
-                if kw=="onPage":
-                    break
-                if kw == "edited":
-                    break
-                kw_clean.append(kw.replace("_",""))
-            photos.append(Photo_Entity(fileName,kw_clean))
+        if ".jpg" in files:
+            for d in et.get_tags([filePath], ["IPTC:Keywords"]):
+                keywords = d.get("IPTC:Keywords", [])
+                if isinstance(keywords, str):
+                    keywords = [keywords]
+                fullKeys = []
+                for kw in keywords:
+                    if kw in ("onPage", "Edited") or "_" in kw:
+                        continue
+                    else:
+                        fullKeys.append(kw)
+                photos[fileName] = (Photo_Entity(fileName[0],fullKeys))
+            else:
+                continue
 
-Photo_Entity.dumpToJson(photos)
-
-# for localKeys in photos:
-#     print(localKeys.getAllKeywords()) # Making sure I can get all local keys out of one file
-
-all_keys = set() # note that the set is a built-in method for ensuring uniqueness in a list
-
-for k in photos:
+for k in photos.values():
     for keyword in k.getAllKeywords():
         all_keys.add(keyword)
 
-keyDict = {} 
-
-
-for k in photos:
+for k in photos.values():
     for keyword, stopNum in k.returnKeywordStopNumberPair():
         if keyword not in keyDict:
             keyDict[keyword] = (Keyword_Entity(keyword, stopNum))
             print("New keyword created:", keyword)
-            keyDict[keyword].addStop(stopNum)
+            # keyDict[keyword].addStop(stopNum)
         else: 
-            keyDict.get(keyword).addStop([stopNum])
+            keyDict.get(keyword).addStop(stopNum)
             print("Added to existing)")
 
+Photo_Entity.dumpToJson(photos)
+Keyword_Entity.dumpKeysToJson(keyDict)
 
-for keys in keyDict:
-    print(keyDict[keys].getStopsFromKeyword())
+for p in photos.values():
+    ceil = random.randint(5,15)
+    loremText = []
+    while ceil >= 0:
+        loremText.append(lorem.sentence()*random.randint(3,12))
+        ceil -= 1
+    with open(f"{sourceFilepath}Stop Number {p.getStopNumber()}.md", "w") as f:
+        f.write(f"# Stop {p.getStopNumber()}\n\n")
+        f.write(f"![[{p.getStopNumber()}.jpg]]\n\n")
+        f.write(f"## Notes\n\n\n## Keywords\n\n")
+        for kw in p.getAllKeywords():
+            f.write(f"- [[{kw}]]\n")
+
+for k in keyDict.values():
+    ceil = random.randint(5,15)
+    loremText = []
+    while ceil >= 0:
+        loremText.append(lorem.sentence()*random.randint(3,12))
+        ceil -= 1
+    with open(f"{sourceFilepath}{k.getLabel()}.md", "w") as f:
+        f.write(f"# Stops referencing keyword {k.getLabel()}\n\n")
+        for a,b in zip_longest(loremText, k.returnStops(), fillvalue=""):
+            if a:
+                f.write(str(a) + "\n\n")
+            if b:
+                f.write(f"![[{str(b)}.jpg]]\n[[Stop Number {str(b)}]]\n\n")
---
+++ dictTest.py
@@ -0,0 +1,35 @@
+import os
+from itertools import zip_longest
+import lorem
+
+paragraphs = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Massa sed elementum tempus egestas. Aliquet risus feugiat in ante metus. Donec pretium vulputate sapien nec sagitt",
+]
+stops = [
+    "1234",
+    "14234",
+    "1234534",
+    "1234534",
+    "1234534",
+    "1234534",
+    "5234",
+]
+
+
+
+with open("testOutput.txt", "w") as f:
+    f.write(f"This is the output\n\n")
+    for a,b in zip_longest(paragraphs, stops, fillvalue=""):
+        if a:
+            f.write(str(a) + "\n\n")
+        if b:
+            f.write(str(b) + "\n\n")
+        
+print(lorem.sentence()*11)
